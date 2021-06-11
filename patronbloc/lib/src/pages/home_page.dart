bb8c6b7391f8dd01f7fac82f39440c7598a08d52
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-11 12:03:40 -0500

    Se realiza el consumo a FireBase para recuperar los productos y se realiza la implementacion en la pagina para dibujar o mostrar los productos.

--- patronbloc/lib/src/pages/home_page.dart
+++ patronbloc/lib/src/pages/home_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:patronbloc/src/models/product_model.dart';
+import 'package:patronbloc/src/providers/productos_provider.dart';
 //import 'package:patronbloc/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
   //const HomePage({Key key}) : super(key: key);
-
+  final productoProvider = new ProductsProvider();
   @override
   Widget build(BuildContext context) {
     //final bloc = ProviderInheritedWidget.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Container(),
+      body: _createListProduct(),
       floatingActionButton: _createButton(context),
     );
   }
@@ -21,6 +24,38 @@ class HomePage extends StatelessWidget {
       onPressed: () => Navigator.pushNamed(context, 'product'),
       child: Icon(Icons.add),
       backgroundColor: Colors.purple,
+    );
+  }
+
+  _createListProduct() {
+    return FutureBuilder(
+      future: productoProvider.loadProducts(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
+        if (snapshot.hasData) {
+          //Se valida si hay información
+
+          final products = snapshot.data;
+
+          return ListView.builder(
+            itemCount: products!.length,
+            itemBuilder: (context, i) => _createItemProduct(
+                products[i], context), //Se envia una instancia de ProductModel
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _createItemProduct(ProductModel product, BuildContext context) {
+    return ListTile(
+      title: Text('${product.titulo} - ${product.valor}'),
+      subtitle: Text(product.id.toString()),
+      onTap: () => Navigator.pushNamed(context, 'product'),
     );
   }
 }
--- patronbloc/lib/src/providers/productos_provider.dart
+++ patronbloc/lib/src/providers/productos_provider.dart
@@ -10,20 +10,51 @@ class ProductsProvider {
 
 //Insertar Productos.
   Future<bool> createProduct(ProductModel product) async {
-    print('createProduct');
-    print(_urlFirebase);
     final _uriS = Uri.https(_urlFirebase, 'productos.json');
-    print(_uriS);
+
     http.Response response = await http.post(_uriS,
         //headers: {"Content-Type": "application/json"},
         body: productModelToJson(product)); //retorna un future
-
-    //
 
     final decodedData = json.decode(response.body);
 
     print(decodedData);
 
     return true;
+  }
+
+  Future<List<ProductModel>> loadProducts() async {
+    final _uriS = Uri.https(_urlFirebase, 'productos.json');
+
+    http.Response response = await http.get(_uriS); //retorna un future
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    final List<ProductModel> products = [];
+
+    if (decodedData.isEmpty)
+      return []; //Validación de la información de la respuesta.
+
+    print('decodedData      : ${decodedData.length}');
+
+    decodedData.forEach((id, prodd) {
+      print('id      : $id');
+      print('prodd : $prodd');
+
+      final prodTmp = ProductModel.fromJson(
+          prodd); //convierte el json en un objeto de tipo ProductModel
+
+      prodTmp.id = id; //Id que retorna FireBase.
+
+      print('prodTmp : $prodTmp');
+
+      products.add(prodTmp); //Se agrega el producto al List<ProductModel>
+
+      print('products : $products');
+      print('products : ${products.length}');
+    });
+
+    print('products : $products');
+
+    return products;
   }
 }
